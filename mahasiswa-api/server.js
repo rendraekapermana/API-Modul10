731f5a763b9ada5328fbea0cf2be6075d3bc2687
Rendra Eka Permana <126854161+rendraekapermana@users.noreply.github.com>
2025-05-13 13:20:29 +0700

    update

--- mahasiswa-api/server.js
+++ mahasiswa-api/server.js
@@ -1,21 +1,23 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const mahasiswaRoutes = require("./routes/mahasiswa");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
-app.use("/mahasiswa", mahasiswaRoutes);
-
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGO_URI, {
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost:27017/mahasiswa",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  }
+);
+
+app.use(cors());
+app.use(express.json()); // <- WAJIB AGAR BODY JSON DIBACA
+app.use("/mahasiswa", mahasiswaRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
